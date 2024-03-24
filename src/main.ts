6098fea31985465ae1a11ee7c3e009c70449b660
riwanab <riwanab@gmail.com>
2024-03-24 17:56:14 +0100

    Fix router to hash mode for Github Pages navigation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createWebHistory, createRouter } from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
 import routes from './router/routes';
 import './style.css'
 import App from './App.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 })
 
